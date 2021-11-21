f681eefe7bddcda3423cefae530dbc21a23e6e32
laubernal <lbernalrodrguez@gmail.com>
2021-11-21 11:22:18 +0100

    removing files from the create react app installation

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+    return (
+      <div>
+        <h1>Hi!</h1>
+      </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+export default App;
+
+
