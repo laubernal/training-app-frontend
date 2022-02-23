b462406a9f178c528bd7850a071487898c21c4f9
laubernal <lbernalrodrguez@gmail.com>
2022-02-23 13:09:53 +0100

    implementing axios

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { SignIn, SignUp } from './Auth/index';
 import { TopNavBar } from './components/index';
-import './App.css';
 import { Home } from './Home/Home';
 import { NewTraining } from './Trainings/index';
+import './App.css';
+import { Categories } from './Trainings/Categories';
 
 export const App = () => {
   return (
@@ -19,6 +20,7 @@ export const App = () => {
           <Route path="/signin/*" element={<SignIn />} />
           <Route path="/signup/*" element={<SignUp />} />
           <Route path="/new/*" element={<NewTraining />} />
+          <Route path="/categories/" element={<Categories/>}/>
         </Routes>
       </div>
     </Router>
--- src/Auth/SignIn.tsx
+++ src/Auth/SignIn.tsx
@@ -1,25 +1,60 @@
 import * as React from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
-import { Button, SectionTitle } from '../components/index';
+import { Button, SectionTitle, Input } from '../components/index';
 import { SignUp } from './index';
 import './Auth.css';
+import axios from 'axios';
 
 export const SignIn = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const signIn = async () => {
+    await axios.post('/signin', { email, password }).then(response => {
+      console.log('response data', response.data);
+    });
+  };
+
   return (
     <form className="ui form login">
       <SectionTitle text="Sign in" /> <br />
       <div className="field required">
         <label>Email</label>
         <div className="ui left icon input">
-          <input autoFocus type="text" placeholder="Enter your email" />
+          <Input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            handleInputChange={handleInputChange}
+          />
+          {/* <input autoFocus type="text" placeholder="Enter your email" /> */}
           <i className="at icon"></i>
         </div>
       </div>
       <div className="field required">
         <label>Password</label>
         <div className="ui left icon input">
-          <input type="password" placeholder="Enter your password" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            handleInputChange={handleInputChange}
+          />
+          {/* <input type="password" placeholder="Enter your password" /> */}
           <i className="lock icon"></i>
         </div>
       </div>
@@ -34,7 +69,10 @@ export const SignIn = (): JSX.Element => {
       </div>
       <Button
         text="Sign in"
-        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => console.log('clicked')}
+        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          console.log('clicked');
+          signIn();
+        }}
       />
     </form>
   );
